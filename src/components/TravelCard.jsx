329d2e48c101f8062ae1974495903ba6385d0afc
Tianyi Xue <tianyixue@MacBookPro.home>
2026-08-27 11:55:13 +0200

    polish style of the travelcard

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -1,21 +1,29 @@
 import { Link } from "react-router-dom";
 
-function TravelCard({ id, travelLocation, travelImage, nameofTraveller,accomodationType }) {
+function TravelCard({
+  id,
+  travelLocation,
+  travelImage,
+  nameofTraveller,
+  accomodationType,
+}) {
   return (
-    <div className="w-80 bg-white rounded-xl overflow-hidden shadow-md hover:shadow-xl transition">
+    <div className="w-60 bg-white rounded-xl overflow-hidden shadow-md hover:shadow-xl transition">
       <Link to={`/travelPlans/${id}`}>
-        <img
-          className="w-full h-48 object-cover"
-          src={travelImage}
-          alt={travelLocation}
-        />
+        <div className="relative w-full h-80">
+          <img
+            className="w-full h-full object-cover"
+            src={travelImage}
+            alt={travelLocation}
+          />
 
-        <div className="p-5">
-          <h2 className="text-xl font-bold text-gray-900">{travelLocation}</h2>
+          <div className="absolute inset-0 flex flex-col justify-end px-5 py-4 text-white">
+            <h2 className="text-xl font-bold">{travelLocation}</h2>
 
-          <p className="text-gray-500 mt-2">Travelled by {nameofTraveller}</p>
+            <p className="mt-[12px]">Travelled by {nameofTraveller}</p>
 
-          <p className="text-gray-600 mt-3">{accomodationType}</p>
+            <p className="mt-[2px]">{accomodationType}</p>
+          </div>
         </div>
       </Link>
     </div>
